9b89c9b8d2bde6e49a8f80679b5c102b3684d72b
borderzz <nickatkins@verizon.net>
2021-06-09 09:20:20 -0400

    redid slowmode cause it didnt work

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,17 +1,22 @@
+const { execute } = require("./ban");
+
 module.exports = {
     name: 'slowmode',
-    description: 'sets a slowmode in the specified channel', 
+    description: 'sets the channels slowmode',
     execute(message, args, Discord) {
-        if(!args[1]) return message.channel.send("You have to give a number for the slowmode (0-21600 seconds)")
-        let duration = args[1]
-        message.channel.setRateLimitPerUser(duration)
-          .catch(() => {
-        message.channel.send("I can't set that slowmode.")
-          })
+        if(!msg.member.hasPermission('MANAGE_CHANNEL')) return;
+        if(!args[0]) return msg.reply('Please specify a time for the channels slowmode (0 - 21600 seconds).');
+        if(isNaN(args[0])) return msg.reply('Please specify a valid time for the channels slowmode.');
+
+        if(args[0] < 0) return msg.reply('The channels slowmode needs to be a positive number.');
+        if(args[0] > 21600) return msg.reply('The slowmode needs to be less than 6 hours (21600 seconds).')
+        msg.channel.setRateLimit(time)
+
         //make embed
         const slowmodeEmbed = new Discord.MessageEmbed()
         .setColor('00FFE4')
-        .setTitle('Set Slowmode')
-        .setDescription(`Slowmode set to " + duration + " seconds.`)
+        .setTitle('Set this channels slowmode.')
+        .setDescription(`I have set the slowmode to \'${time}\' seconds.`)
+        message.channel.send(slowmodeEmbed)
     }
 }
