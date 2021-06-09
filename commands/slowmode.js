5e5bef8acd92da0926141cf0458ced73d7ba1094
borderzz <nickatkins@verizon.net>
2021-06-09 09:26:25 -0400

    had to tell what time is

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -7,7 +7,7 @@ module.exports = {
         if(!message.member.hasPermission('MANAGE_CHANNEL')) return;
         if(!args[0]) return message.reply('Please specify a time for the channels slowmode (0 - 21600 seconds).');
         if(isNaN(args[0])) return message.reply('Please specify a valid time for the channels slowmode.');
-
+        var time = args[0]
         if(args[0] < 0) return message.reply('The channels slowmode needs to be a positive number.');
         if(args[0] > 21600) return message.reply('The slowmode needs to be less than 6 hours (21600 seconds).')
         message.channel.setRateLimit(time)
