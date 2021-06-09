d556e73e3590786a722fed6807fc9572e5b23ff7
borderzz <nickatkins@verizon.net>
2021-06-08 21:42:10 -0400

    link on invite

--- commands/invite.js
+++ commands/invite.js
@@ -6,6 +6,7 @@ module.exports = {
         const inviteEmbed = new Discord.MessageEmbed()
         .setColor('00FFE4')
         .setTitle('Invite this bot to your server')
+        .setURL('https://discord.com/api/oauth2/authorize?client_id=851899997430480947&permissions=8&scope=bot')
         .setDescription('If you want to use this bot for yourself, just click the link above!')
 
         //send message 
