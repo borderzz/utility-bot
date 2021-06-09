75aa368f877fddf06cbba7ce417d75fcb7158964
borderzz <nickatkins@verizon.net>
2021-06-08 21:50:54 -0400

    name changed to username cause it didnt work

--- commands/kick.js
+++ commands/kick.js
@@ -12,7 +12,7 @@ module.exports = {
             //embed for kicking member
             const kickEmbed = new Discord.MessageEmbed()
             .setColor('FF0000')
-            .setTitle(`${user.tag} was kicked by ${message.authour.name}.`)
+            .setTitle(`${user.tag} was kicked by ${message.authour.username}.`)
             .setDescription('Rekt')
             message.reply(kickEmbed);
           })
