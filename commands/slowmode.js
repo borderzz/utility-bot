aae07e65125cf5406609c4b954ecc675c4f7d6d5
borderzz <nickatkins@verizon.net>
2021-06-09 09:23:16 -0400

    msg - message

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -4,13 +4,13 @@ module.exports = {
     name: 'slowmode',
     description: 'sets the channels slowmode',
     execute(message, args, Discord) {
-        if(!msg.member.hasPermission('MANAGE_CHANNEL')) return;
-        if(!args[0]) return msg.reply('Please specify a time for the channels slowmode (0 - 21600 seconds).');
-        if(isNaN(args[0])) return msg.reply('Please specify a valid time for the channels slowmode.');
+        if(!message.member.hasPermission('MANAGE_CHANNEL')) return;
+        if(!args[0]) return message.reply('Please specify a time for the channels slowmode (0 - 21600 seconds).');
+        if(isNaN(args[0])) return message.reply('Please specify a valid time for the channels slowmode.');
 
-        if(args[0] < 0) return msg.reply('The channels slowmode needs to be a positive number.');
-        if(args[0] > 21600) return msg.reply('The slowmode needs to be less than 6 hours (21600 seconds).')
-        msg.channel.setRateLimit(time)
+        if(args[0] < 0) return message.reply('The channels slowmode needs to be a positive number.');
+        if(args[0] > 21600) return message.reply('The slowmode needs to be less than 6 hours (21600 seconds).')
+        message.channel.setRateLimit(time)
 
         //make embed
         const slowmodeEmbed = new Discord.MessageEmbed()
