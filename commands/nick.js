34e23d725db5af59dcd161d065d4a62b3d39bb6d
borderzz <nickatkins@verizon.net>
2021-06-11 21:22:35 -0400

    fixed nick command

--- commands/nick.js
+++ commands/nick.js
@@ -2,20 +2,22 @@ module.exports = {
     name: 'nick',
     description: 'gives the bot a nickname',
     execute(message, args, Discord) {
-        let user = message.mentions.users.first()
-    if(!user) return message.reply("Please mention a user to change their username!")
+        let nickedUser = message.mentions.users.first();
 
-    let nickname = args.slice(1).join(" ")
-    if(!nickname) return message.reply("Specify a nickname to change to!")
+        let newNick = args.join(" ").slice(22);
 
-    let member = message.guild.members.cache.get(user.id);
-    member.setNickname(nickname);
+        if(!nickedUser) return message.channel.send("You have to mention a user to nick!");
 
-    const nickEmbed = new Discord.MessageEmbed()
-    .setTitle("Nickname Changed")
-    .setDescription(`successfully changed ${user.tag}'s nickname to ${nickname}`)
-    .setColor('00FFE4')
-    .setTimestamp()
-    message.channel.send(nickEmbed);
+        if(!newNick) return message.channel.send("That is not a valid name!");
+        
+        if(!message.member.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("You dont have the permission to change nicknames!");
+
+        nickedUser.setNickname(newNick);
+        //make success embed
+        const nickEmbed = new Discord.MessageEmbed()
+        .setColor('00FFE4')
+        .setTitle('User was nicked')
+        .setDescription(`Changed ${nickedUser}'s nickname to ${newNick}!`)
+        .setTimestamp()
     }
 }
