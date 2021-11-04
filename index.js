4a2707a3f3c895c7a010c37cf3a52e6a76fa434e
borderzz <nickatkins@verizon.net>
2021-11-04 18:12:57 -0400

    asdfadsf

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ let canread = true;
 let a = 1
 
 //check if message is bot + starts with prefix
-while (a = 1) {
+for (;;) {
 if(canread == true) {
 client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
